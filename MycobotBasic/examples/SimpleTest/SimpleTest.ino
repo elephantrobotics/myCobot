0cbf7717555950876ac155bd102d9ff064728804
elephantrobotics <60350177+elephantrobotics@users.noreply.github.com>
2020-11-25 15:00:55 +0800

    Change path

--- MycobotBasic/examples/SimpleTest/SimpleTest.ino
+++ MycobotBasic/examples/SimpleTest/SimpleTest.ino
@@ -14,11 +14,11 @@ void setup() {
 void loop() {
   for(int i = 1; i < 7; i++)        //run
     {
-      myCobot.setAngle(i, -50, 3000);
+      myCobot.setAngle(i, -50, 3000);   // set joint angle - 50 degree
       delay(2000);
-      myCobot.setAngle(i, 50, 3000);
+      myCobot.setAngle(i, 50, 3000);    // set joint angle 50 degree
       delay(2000);
-      myCobot.setAngle(i, 0, 3000);
+      myCobot.setAngle(i, 0, 3000);   // set joint angle 0 degree
       delay(2000);
     }
 }
