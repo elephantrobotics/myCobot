016ed551a8c1b41c07cf6b94b1b6b04c4876ca39
wxy6655 <74954671+wxy6655@users.noreply.github.com>
2021-04-21 14:30:05 +0800

    Update setGripper.ino

--- Arduino/MycobotBasic/examples/setGripper/setGripper.ino
+++ Arduino/MycobotBasic/examples/setGripper/setGripper.ino
@@ -1,5 +1,5 @@
 /// 此文件用于mycobot设备控制末端夹爪运动，对应烧录设备为Basic
-/// 烧炉前请将夹爪张开至最大角度
+/// 烧录前请将夹爪张开至最大角度
 #include <MycobotBasic.h>
 #include <ParameterList.h>
 
@@ -19,7 +19,9 @@ void loop() {
     myCobot.setEncoder(7,2048);                     //设定末端夹爪打开至2048电位值
     delay(2000);
     Serial.println(myCobot.getGripperValue());      //读取末端夹爪电位值
+    delay(50);
     myCobot.setEncoder(7,1700);                     //设定末端夹爪打开至2048电位值
     delay(2000);
     Serial.println(myCobot.getGripperValue());      //读取末端夹爪电位值
+    delay(50);
 }
